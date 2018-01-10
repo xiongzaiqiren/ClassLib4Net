ef0c59642acc48777c6e95deb0409d6df05592b3
熊仔其人 <xiongzaiqiren@163.com>
2018-01-10 17:29:37 +0800

    ApiModel 以及 ApiController 已完成

--- ClassLib4Net/Api/ApiModel.cs
+++ ClassLib4Net/Api/ApiModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -8,150 +9,261 @@ using System.Text;
 namespace ClassLib4Net.Api
 {
     #region interface
+    /// <summary>
+    /// 分页参数
+    /// </summary>
     public interface IPaging
     {
-        int PageSize { get; set; }
-        int PageIndex { get; set; }
+        /// <summary>
+        /// 页容量
+        /// </summary>
+        long PageSize { get; set; }
+        /// <summary>
+        /// 页索引
+        /// </summary>
+        long PageIndex { get; set; }
     }
+    /// <summary>
+    /// 分页总数
+    /// </summary>
+    public interface IPageCount
+    {
+        /// <summary>
+        /// 分页总数
+        /// </summary>
+        long PageCount { get; set; }
+    }
+    /// <summary>
+    /// 总数
+    /// </summary>
     public interface ITotal
     {
-        int Total { get; set; }
+        /// <summary>
+        /// 总数
+        /// </summary>
+        long Total { get; set; }
     }
+    /// <summary>
+    /// 状态信息
+    /// </summary>
     public interface IBase
     {
-        int Status { get; set; }
+        /// <summary>
+        /// 状态码
+        /// </summary>
+        long Status { get; set; }
+        /// <summary>
+        /// 信息
+        /// </summary>
         string Message { get; set; }
     }
     #endregion
 
+    #region api data
+    /// <summary>
+    /// 基本接口模型
+    /// </summary>
     [Serializable]
     [DataContract]
     public class ApiModel : IBase
     {
+        /// <summary>
+        /// 状态码
+        /// </summary>
         [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int Status { get; set; }
-
+        public long Status { get; set; }
+        /// <summary>
+        /// 信息
+        /// </summary>
         [DataMember(EmitDefaultValue = true, IsRequired = true)]
         public string Message { get; set; }
 
-        public ApiModel(int status = 0, string message = "")
-        {
-            Status = status;
-            Message = message;
-        }
+        public ApiModel() { }
+        public ApiModel(long status = 0, string message = "") { Status = status; Message = message; }
     }
 
+    /// <summary>
+    /// 数据接口模型
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     [Serializable]
     [DataContract]
     public class ApiDataModel<T> : ApiModel
     {
+        /// <summary>
+        /// 数据
+        /// </summary>
         [DataMember(EmitDefaultValue = true, IsRequired = true)]
         public T Data { get; set; }
 
-        public ApiDataModel(T data = default(T), int status = 0, string message = "") : base(status, message)
-        {
-            Data = data;
-        }
+        public ApiDataModel() { }
+        public ApiDataModel(T data = default(T), long status = 0, string message = "") : base(status, message) { Data = data; }
     }
 
+    /// <summary>
+    /// 数据接口模型
+    /// </summary>
     [Serializable]
     [DataContract]
     public class ApiDataModel : ApiDataModel<object>
     {
-        public ApiDataModel(object data = null, int status = 0, string message = "") : base(data, status, message) { }
-    }
-
-    #region internal
-
-    [Serializable]
-    [DataContract]
-    public class ApiListData<T> : IPaging, ITotal
-    {
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public IList<T> List { get; set; }
-
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int PageSize { get; set; }
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int PageIndex { get; set; }
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int Total { get; set; }
-
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int PageCount { get { return (Total - 1) / PageSize + 1; } }
-
-        internal ApiListData() { }
-    }
-
-    [Serializable]
-    [DataContract]
-    public class ApiListData : IPaging, ITotal
-    {
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public IList List { get; set; }
-
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int PageSize { get; set; }
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int PageIndex { get; set; }
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int Total { get; set; }
-
-
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int PageCount { get { return (Total - 1) / PageSize + 1; } }
-
-        internal ApiListData() { }
+        public ApiDataModel() : base() { }
+        public ApiDataModel(object data = null, long status = 0, string message = "") : base(data, status, message) { }
     }
     #endregion
 
+    #region api list
+    /// <summary>
+    /// 列表数据接口模型
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     [Serializable]
     [DataContract]
-    public class ApiDataListModel<T> : ApiDataModel<ApiListData<T>>
+    public class ApiListModel<T> : ApiModel, IPaging, IPageCount, ITotal
     {
-        public ApiDataListModel(IList<T> list = null, int total = 0) : base(new ApiListData<T>() { List = list ?? new List<T>(), Total = total }) { }
+        /// <summary>
+        /// 列表数据
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public ICollection<T> List { get; set; }
+        /// <summary>
+        /// 页容量
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long PageSize { get; set; }
+        /// <summary>
+        /// 页索引
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long PageIndex { get; set; }
+        /// <summary>
+        /// 总数
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long Total { get; set; }
+
+        /// <summary>
+        /// 分页总数(根据总数和页容量自动计算)
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long PageCount { get { return (Total - 1) / PageSize + 1; } set { } }
+
+        public ApiListModel() { }
+        public ApiListModel(ICollection<T> list = null, long total = 0, long status = 0, string message = "") : base(status, message) { Total = total; List = list; }
     }
 
-    [Serializable]
-    [DataContract]
-    public class ApiDataListModel : ApiDataModel<ApiListData>
-    {
-        public ApiDataListModel(IList list, int total = 0) : base(new ApiListData() { List = list, Total = total }) { }
-    }
-
-    [Serializable]
-    [DataContract]
-    public class ApiListModel<T> : ApiModel, IPaging, ITotal
-    {
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public IList<T> List { get; set; }
-
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int PageSize { get; set; }
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int PageIndex { get; set; }
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int Total { get; set; }
-
-
-        [DataMember(EmitDefaultValue = true, IsRequired = true)]
-        public int PageCount { get { return (Total - 1) / PageSize + 1; } }
-
-        public ApiListModel(IList<T> list = null, int total = 0, int status = 0, string message = "") : base(status, message)
-        {
-            Total = total;
-            List = list ?? new List<T>();
-        }
-    }
-
+    /// <summary>
+    /// 列表数据接口模型
+    /// </summary>
     [Serializable]
     [DataContract]
     public class ApiListModel : ApiListModel<object>
     {
-        public ApiListModel(IList<object> list = null, int total = 0, int status = 0, string message = "") : base(list, total, status, message) { }
+        public ApiListModel() : base() { }
+        public ApiListModel(ICollection<object> list = null, long total = 0, long status = 0, string message = "") : base(list, total, status, message) { }
+    } 
+    #endregion
+
+    #region ApiListData
+    /// <summary>
+    /// 列表数据类型
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [Serializable]
+    [DataContract]
+    public class ApiListData<T> : IPaging, IPageCount, ITotal
+    {
+        /// <summary>
+        /// 列表数据
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public ICollection<T> List { get; set; }
+        /// <summary>
+        /// 页容量
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long PageSize { get; set; }
+        /// <summary>
+        /// 页索引
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long PageIndex { get; set; }
+        /// <summary>
+        /// 总数
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long Total { get; set; }
+
+        /// <summary>
+        /// 分页总数(根据总数和页容量自动计算)
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long PageCount { get { return (Total - 1) / PageSize + 1; } set { } }
+
+        public ApiListData() { }
+        public ApiListData(ICollection<T> list = null, long total = 0) { List = list; Total = total; }
     }
 
+    /// <summary>
+    /// 列表数据类型
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class ApiListData : IPaging, IPageCount, ITotal
+    {
+        /// <summary>
+        /// 列表数据
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public ICollection List { get; set; }
+        /// <summary>
+        /// 页容量
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long PageSize { get; set; }
+        /// <summary>
+        /// 页索引
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long PageIndex { get; set; }
+        /// <summary>
+        /// 总数
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long Total { get; set; }
 
+        /// <summary>
+        /// 分页总数(根据总数和页容量自动计算)
+        /// </summary>
+        [DataMember(EmitDefaultValue = true, IsRequired = true)]
+        public long PageCount { get { return (Total - 1) / PageSize + 1; } set { } }
 
+        public ApiListData() { }
+        public ApiListData(ICollection list = null, long total = 0) { List = list; Total = total; }
+    }
+    #endregion
+
+    /// <summary>
+    /// 数据列表接口模型
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [Serializable]
+    [DataContract]
+    public class ApiDataListModel<T> : ApiDataModel<ApiListData<T>>
+    {
+        public ApiDataListModel() : base() { Data = new ApiListData<T>(); }
+        public ApiDataListModel(ICollection<T> list = null, long total = 0) : base(new ApiListData<T>() { List = list, Total = total }) { }
+    }
+
+    /// <summary>
+    /// 数据列表接口模型
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class ApiDataListModel : ApiDataModel<ApiListData>
+    {
+        public ApiDataListModel() : base() { Data = new ApiListData(); }
+        public ApiDataListModel(ICollection list = null, long total = 0) : base(new ApiListData() { List = list, Total = total }) { }
+    }
 
 }
---
+++ TestWebApplication/Controllers/ApiController.cs
@@ -0,0 +1,124 @@
+﻿using ClassLib4Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWebApplication.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+
+        public JsonResult Get()
+        {
+            var result = new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet, ContentEncoding = System.Text.Encoding.UTF8 };
+            result.Data = new ClassLib4Net.Api.ApiModel()
+            {
+                Status = 200,
+                Message = "ok"
+            };
+
+            return result;
+        }
+
+        public class PeopleModel
+        {
+            public string Name { get; set; }
+            public string Nationality { get; set; }
+            public string Province { get; set; }
+            public int Age { get; set; }
+        }
+
+        public JsonResult GetData()
+        {
+            var result = new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet, ContentEncoding = System.Text.Encoding.UTF8 };
+            result.Data = new ClassLib4Net.Api.ApiDataModel()
+            {
+                Data = new PeopleModel() { Name = "熊仔其人", Nationality = "中国", Province = "北京", Age = 25 },
+                Status = 200,
+                Message = "ok"
+            };
+
+            return result;
+        }
+        public JsonResult GetList()
+        {
+            var result = new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet, ContentEncoding = System.Text.Encoding.UTF8 };
+            result.Data = new ClassLib4Net.Api.ApiListModel<PeopleModel>
+            {
+                List = new List<PeopleModel>() {
+                     new PeopleModel(){ Name = "熊仔其人", Nationality = "中国", Province = "北京", Age = 25 },
+                     new PeopleModel(){ Name = "xiongzaiqiren", Nationality = "China", Province = "Beijing", Age = 25 },
+                     new PeopleModel(){ Name = "熊学浩", Nationality = "中国", Province = "河南" , Age = 25}
+                 },
+                PageIndex = 1,
+                PageSize = 2,
+                Total = 3,
+                Status = 200,
+                Message = "ok"
+            };
+
+            return result;
+        }
+        public JsonResult GetDataList()
+        {
+            var result = new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet, ContentEncoding = System.Text.Encoding.UTF8 };
+
+            var entity = new ClassLib4Net.Api.ApiDataListModel();
+            entity.Data.List = new List<PeopleModel>(){
+                         new PeopleModel(){ Name = "熊仔其人", Nationality = "中国", Province = "北京" },
+                         new PeopleModel(){ Name = "xiongzaiqiren", Nationality = "China", Province = "Beijing" },
+                         new PeopleModel(){ Name = "熊学浩", Nationality = "中国", Province = "河南" }
+                     };
+            entity.Data.PageIndex = 1;
+            entity.Data.PageSize = 2;
+            entity.Data.Total = 3;
+
+            entity.Status = 200;
+            entity.Message = "ok";
+
+            result.Data = entity;
+            return result;
+        }
+
+        /// <summary>
+        /// 解析基础接口模型
+        /// </summary>
+        public void Read()
+        {
+            string s = ClassLib4Net.Http.HttpHelper.Request(Request.Url.Scheme + "://" + Request.Url.Host + "/Api/Get");
+            var json1 = s.ToObject<ClassLib4Net.Api.ApiModel>();
+            var json2 = JsonHelper.DeSerialize<ClassLib4Net.Api.ApiModel>(s);
+        }
+        /// <summary>
+        /// 解析数据接口模型
+        /// </summary>
+        public void ReadData()
+        {
+            string s = ClassLib4Net.Http.HttpHelper.Request(Request.Url.Scheme + "://" + Request.Url.Host + "/Api/GetData");
+            var json1 = s.ToObject<ClassLib4Net.Api.ApiDataModel<PeopleModel>>();
+            var json2 = JsonHelper.DeSerialize<ClassLib4Net.Api.ApiDataModel<PeopleModel>>(s);
+        }
+        /// <summary>
+        /// 解析列表数据接口模型
+        /// </summary>
+        public void ReadList()
+        {
+            string s = ClassLib4Net.Http.HttpHelper.Request(Request.Url.Scheme + "://" + Request.Url.Host + "/Api/GetList");
+            var json1 = s.ToObject<ClassLib4Net.Api.ApiListModel<PeopleModel>>();
+            var json2 = JsonHelper.DeSerialize<ClassLib4Net.Api.ApiListModel<PeopleModel>>(s);
+        }
+        /// <summary>
+        /// 解析数据列表接口模型
+        /// </summary>
+        public void ReadDataList()
+        {
+            string s = ClassLib4Net.Http.HttpHelper.Request(Request.Url.Scheme + "://" + Request.Url.Host + "/Api/GetDataList");
+            var json1 = s.ToObject<ClassLib4Net.Api.ApiDataListModel<PeopleModel>>();
+            var json2 = JsonHelper.DeSerialize<ClassLib4Net.Api.ApiDataListModel<PeopleModel>>(s);
+        }
+
+    }
+}
